e8af7b3746ec94d258580d14171634179332534b
AdrianCamilleri <adriancamilleri19@gmail.com>
2020-05-27 01:21:34 +0200

    Added remove line with only terminators

--- src/extension.ts
+++ src/extension.ts
@@ -7,11 +7,44 @@ export function activate(context: vscode.ExtensionContext) {
 	let editProvider = {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
 			console.log('Terminator - EXECUTE: format');
-			const firstLine = document.lineAt(0);
-			if (firstLine.text !== '42') {
-				return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
+
+			let textEditList = [];
+
+			let terminators = [';', '{', '}'];
+			let indentSize = 4;
+
+			function escapeRegExp(text: string) {
+				return text.replace(/[-[\]{}()*+?.,\\^$|#\\s]/g, '\\$&');
 			}
-			return [];
+
+			let terminatorsRegex = '';
+			for (let i = 0; i < terminators.length; i++) {
+				const terminator = terminators[i];
+				terminatorsRegex += escapeRegExp(terminator);
+			}
+			terminatorsRegex = '[' + terminatorsRegex + ']';
+
+			// Remove line with only terminators
+			for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+				const line = document.lineAt(lineIndex);
+				if (line && line.text) {
+					let match = line.text.match(new RegExp('(\\s*)(' + terminatorsRegex + ')'));
+					if (match && match[1]) {
+						// If possible remove newline
+						let previousLine;
+						if (lineIndex > 0) {
+							previousLine = document.lineAt(lineIndex - 1);
+						}
+						if (previousLine && previousLine.text) {
+							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex - 1, previousLine.text.length, lineIndex, match[1].length)));
+						} else {
+							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex, 0, lineIndex, match[1].length)));
+						}
+					}
+				}
+			}
+
+			return textEditList;
 		}
 	};
 
