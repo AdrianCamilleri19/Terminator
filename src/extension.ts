e33ccaeb70a66cde3aaf2bcc5e57a8fb0f89926e
AdrianCamilleri <adriancamilleri19@gmail.com>
2020-05-27 00:37:23 +0200

    Setup formatting API

--- src/extension.ts
+++ src/extension.ts
@@ -1,27 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "terminator" is now active!');
+	console.log('Terminator - ONLINE');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('terminator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let editProvider = {
+		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
+			console.log('Terminator - EXECUTE: format');
+			const firstLine = document.lineAt(0);
+			if (firstLine.text !== '42') {
+				return [vscode.TextEdit.insert(firstLine.range.start, '42\n')];
+			}
+			return [];
+		}
+	};
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Terminator!');
-	});
-
-	context.subscriptions.push(disposable);
+	vscode.languages.registerDocumentFormattingEditProvider('csharp', editProvider);
+	vscode.languages.registerDocumentFormattingEditProvider('javascript', editProvider);
 }
-
-// this method is called when your extension is deactivated
-export function deactivate() {}
