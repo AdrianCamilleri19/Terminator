0f70af1b3661cc3566492324b9e9c09405237e10
AdrianCamilleri <adriancamilleri19@gmail.com>
2020-05-27 20:59:07 +0200

    Added support for terminators before code

--- src/extension.ts
+++ src/extension.ts
@@ -42,34 +42,47 @@ export function activate(context: vscode.ExtensionContext) {
 			// Offset
 			maxIndent += indentSize;
 
-			// TODO: handle terminators at start of line eg: "} else {"
-			// TODO: handle whitespace after terminators
+			let lineRegex = new RegExp('(^\\s*)(' + terminatorsRegex + '*)(.*?)(' + terminatorsRegex + '*)(\\s*?$)');
 			for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
 				const line: vscode.TextLine = document.lineAt(lineIndex);
 				if (line && line.text) {
-					let onlyTerminatorLineMatch = line.text.match(new RegExp('(^\\s*)(' + terminatorsRegex + ')+$'));
-					if (onlyTerminatorLineMatch && onlyTerminatorLineMatch[1] != null) {
-						// Line with terminator and whitespace only
-						let previousLine;
-						if (lineIndex > 0) {
-							previousLine = document.lineAt(lineIndex - 1);
-						}
-						if (previousLine && previousLine.text) {
-							// Add padding if previousLine doesn't end with a terminator
-							if (!new RegExp(terminatorsRegex + '$').test(previousLine.text)) {
-								let padding = ' '.repeat(maxIndent - previousLine.text.length);
-								textEditList.push(vscode.TextEdit.insert(new vscode.Position(lineIndex - 1, previousLine.text.length), padding));
+					let lineMatch = line.text.match(lineRegex);
+					if (lineMatch && lineMatch.length === 6) {
+						let preWhitespaces: string = lineMatch[1];
+						let preTerminators: string = lineMatch[2];
+						let code: string = lineMatch[3];
+						let postTerminators: string = lineMatch[4];
+						let postWhitespaces: string = lineMatch[5];
+
+						if (preTerminators) {
+							let previousLine;
+							if (lineIndex > 0) {
+								previousLine = document.lineAt(lineIndex - 1);
 							}
-							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex - 1, previousLine.text.length, lineIndex, onlyTerminatorLineMatch[1].length)));
-						} else {
-							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex, 0, lineIndex, onlyTerminatorLineMatch[1].length)));
+							if (previousLine && previousLine.text) {
+								// Add padding if previousLine doesn't end with a terminator
+								if (!new RegExp(terminatorsRegex + '$').test(previousLine.text)) {
+									let padding = ' '.repeat(maxIndent - previousLine.text.length);
+									textEditList.push(vscode.TextEdit.insert(new vscode.Position(lineIndex - 1, previousLine.text.length), padding));
+								}
+								textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex - 1, previousLine.text.length, lineIndex, preWhitespaces.length)));
+							} else {
+								textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex, 0, lineIndex, preWhitespaces.length)));
+							}
 						}
-					} else {
-						// Indent code with text and terminator
-						let match = line.text.match(new RegExp('(.*?)(' + terminatorsRegex + '+)$'));
-						if (match && match.length == 3 && match[1]) {
-							let padding = ' '.repeat(maxIndent - match[1].length);
-							textEditList.push(vscode.TextEdit.insert(new vscode.Position(line.lineNumber, match[1].length), padding));
+						if (preTerminators && code) {
+							let prePadding = ' '.repeat(preWhitespaces.length);
+							let codeStartIndex = preWhitespaces.length + preTerminators.length;
+							textEditList.push(vscode.TextEdit.insert(new vscode.Position(line.lineNumber, codeStartIndex), '\n' + prePadding));
+							
+							let postPadding = ' '.repeat(preTerminators.length);
+							let codeEndIndex = preWhitespaces.length + preTerminators.length + code.length;
+							textEditList.push(vscode.TextEdit.insert(new vscode.Position(line.lineNumber, codeEndIndex), postPadding));
+						}
+						if (postTerminators) {
+							let codeEndIndex = preWhitespaces.length + preTerminators.length + code.length;
+							let padding = ' '.repeat(maxIndent - codeEndIndex);
+							textEditList.push(vscode.TextEdit.insert(new vscode.Position(line.lineNumber, codeEndIndex), padding));
 						}
 					}
 				}
