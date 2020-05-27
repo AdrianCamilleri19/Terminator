d1db923c0b6fc8b943dcea84a3d9605d04b3f15e
AdrianCamilleri <adriancamilleri19@gmail.com>
2020-05-27 02:57:30 +0200

    Added indentation

--- src/extension.ts
+++ src/extension.ts
@@ -8,7 +8,7 @@ export function activate(context: vscode.ExtensionContext) {
 		provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
 			console.log('Terminator - EXECUTE: format');
 
-			let textEditList = [];
+			let textEditList: vscode.TextEdit[] = [];
 
 			let terminators = [';', '{', '}'];
 			let indentSize = 4;
@@ -24,21 +24,52 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			terminatorsRegex = '[' + terminatorsRegex + ']';
 
-			// Remove line with only terminators
+			// Get max indent ignoring terminator/whitespace
+			let maxIndent = 0;
 			for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
-				const line = document.lineAt(lineIndex);
+				const line: vscode.TextLine = document.lineAt(lineIndex);
 				if (line && line.text) {
-					let match = line.text.match(new RegExp('(\\s*)(' + terminatorsRegex + ')'));
-					if (match && match[1]) {
-						// If possible remove newline
+					let lineIndentMatch = line.text.match(new RegExp('(.*?)(\\s|' + terminatorsRegex + ')*$'));
+					if (lineIndentMatch && lineIndentMatch.length > 2 && lineIndentMatch[1].length > maxIndent) {
+						maxIndent = lineIndentMatch[1].length;
+					}
+				}
+			}
+			// Increment by 1 to indent after non terminator/whitespace stops
+			maxIndent++;
+			// Indent at indentSize intervals
+			maxIndent += maxIndent % indentSize;
+			// Offset
+			maxIndent += indentSize;
+
+			// TODO: handle terminators at start of line eg: "} else {"
+			// TODO: handle whitespace after terminators
+			for (let lineIndex = 0; lineIndex < document.lineCount; lineIndex++) {
+				const line: vscode.TextLine = document.lineAt(lineIndex);
+				if (line && line.text) {
+					let onlyTerminatorLineMatch = line.text.match(new RegExp('(^\\s*)(' + terminatorsRegex + ')+$'));
+					if (onlyTerminatorLineMatch && onlyTerminatorLineMatch[1] != null) {
+						// Line with terminator and whitespace only
 						let previousLine;
 						if (lineIndex > 0) {
 							previousLine = document.lineAt(lineIndex - 1);
 						}
 						if (previousLine && previousLine.text) {
-							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex - 1, previousLine.text.length, lineIndex, match[1].length)));
+							// Add padding if previousLine doesn't end with a terminator
+							if (!new RegExp(terminatorsRegex + '$').test(previousLine.text)) {
+								let padding = ' '.repeat(maxIndent - previousLine.text.length);
+								textEditList.push(vscode.TextEdit.insert(new vscode.Position(lineIndex - 1, previousLine.text.length), padding));
+							}
+							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex - 1, previousLine.text.length, lineIndex, onlyTerminatorLineMatch[1].length)));
 						} else {
-							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex, 0, lineIndex, match[1].length)));
+							textEditList.push(vscode.TextEdit.delete(new vscode.Range(lineIndex, 0, lineIndex, onlyTerminatorLineMatch[1].length)));
+						}
+					} else {
+						// Indent code with text and terminator
+						let match = line.text.match(new RegExp('(.*?)(' + terminatorsRegex + '+)$'));
+						if (match && match.length == 3 && match[1]) {
+							let padding = ' '.repeat(maxIndent - match[1].length);
+							textEditList.push(vscode.TextEdit.insert(new vscode.Position(line.lineNumber, match[1].length), padding));
 						}
 					}
 				}
